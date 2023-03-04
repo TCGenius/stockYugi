f51472fccdc2d14efbe56f993921667d1ab36fdd
TCGenius <alvarez.andres.g@gmail.com>
2023-03-04 09:18:21 -0300

    Ya puede filtrar el JSON y escribir la base

--- index.js
+++ index.js
@@ -1,37 +1,48 @@
-const { Pool } = require('pg');
-const http = require('http');
+const {pool} = require('./routers/pg.js')
 const express = require('express');
 const app = express();
-//const pgConfig = require('./config/config.js')
+const fs = require('fs');
 
-const pool = new Pool({
-    host: 'localhost',
-    user: 'postgres',
-    password: 'Aconcagua4826',
-    database: 'stockYugi'
-  });
-let ubicacion = [];
-const consultaUbi = async () => {
-        const res = await pool.query('SELECT * FROM ubicaciones');
-        ubicacion = res.rows;
+const routerCartas = require('./routers/cartas.js');
+const routerUbi = require('./routers/ubicaciones.js');
+
+
+
+let ubicacion = [], cartas = []; //Inicializo los arrays que van a tomar valor luego.
+
+//Routers
+app.use('/api/cartas', routerCartas);
+app.use('/api/ubicaciones', routerUbi);
+
+const cargaUbi = async () => {
+	const result = await pool.query('SELECT * FROM ubicaciones');
+	ubicacion = result.rows;
 };
-app.get('/', (req, res) => {
-    res.send('<h1>Algo</h1>');
-})
+const cargaCartas = async () => {
+	const result = await pool.query('SELECT * FROM cartasEnStock');
+	cartas = result.rows;
+};
+
+app.get('/', async (req, res) => {
+		await cargaUbi();
+		await cargaCartas();
+		res.send('Datos cargados')
+	}
+	)
 
 app.get('/ubicacion/:id', async (req, res) => {
-    await consultaUbi();
-    const id = req.params.id
-    console.log(id);
-    const indice = ubicacion.findIndex(result => result.idubi == id);
-    if (indice.length === -1) {
-        return res.status(404).send('Tasfla sheando')
-    }
-    res.send(ubicacion[indice]);
+	await consultaUbi();
+	const id = req.params.id
+	console.log(id);
+	const indice = ubicacion.findIndex(result => result.idubi == id);
+	if (indice.length === -1) {
+			return res.status(404).send('Tasfla sheando')
+	}
+	res.send(ubicacion[indice]);
 })
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-    console.log(`Servidor escuchando en el puerto ${PORT}`)
+	console.log(`Servidor escuchando en el puerto ${PORT}`)
 });
